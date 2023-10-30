4f5be06a3ab6e3fd4cd19866f3475140883d4692
heal333 <rohanaich333@gamil.com>
2023-10-31 02:51:34 +0530

    cart remove button

--- src/components/Layout/headerCartButton.js
+++ src/components/Layout/headerCartButton.js
@@ -1,17 +1,32 @@
-import React from "react";
-import { useContext } from "react";
+import { React,useContext,useEffect,useState } from "react";
 import CartIcon from "../../plugs/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
+  const [btnIsHighLighted, setBtnIsHighlighted] = useState(false)
+
   const cartCtx = useContext(CartContext);
 
   const numberOfCarItems = cartCtx.items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
+  const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump:""}`;
+
+  useEffect(()=>{
+    if (cartCtx.items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(()=>{
+      setBtnIsHighlighted(false);
+    },300);
+    return () =>{
+      clearTimeout(timer);
+    }
+  }, [cartCtx.items])
 
   return (
-    <button className={classes.button} onClick={props.onPress}>
+    <button className={btnClasses} onClick={props.onPress}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
