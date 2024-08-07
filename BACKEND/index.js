cd7277922709198a97599282ae83dd2edcc3be2d
heal333 <rohanaich333@gmail.com>
2024-08-08 00:31:12 +0530

    fixing api

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,33 +7,38 @@ const app = express();
 app.use(cors());
 dotenv.config({ path: "./config.env" });
 
-// mongoose.connect(process.env.DATABASE).then((con) => {
-//     console.log("successfully connected with the database");
-//     app.listen(3000, () => {
-//         console.log("listening on por 300");
-//     });
-// });
+const DB = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+);
 
-// const data = mongoose.model("restaurants", new mongoose.Schema());
-
-// app.get("/", async (req, res) => {
-//     result = await data.find(
-//         {},
-//         {
-//             "Restaurant Name": 1,
-//             Cuisine: 1,
-//             Rating: 1,
-//             "Pure Veg": 1,
-//             "Delivery Time": 1,
-//             Image: 1,
-//         }
-//     );
-//     res.status(200).json(result.slice(0, 20));
-// });
-
-app.listen(3000, () => {
-    console.log("listing on 3k");
+mongoose.connect(DB).then((con) => {
+    console.log("successfully connected with the database");
+    app.listen(3000, () => {
+        console.log("listening on por 300");
+    });
 });
-app.get("/", (req, res) => {
-    res.status(200).json({ yougot: "rooted" });
+
+const data = mongoose.model("restaurants", new mongoose.Schema());
+
+app.get("/", async (req, res) => {
+    result = await data.find(
+        {},
+        {
+            "Restaurant Name": 1,
+            Cuisine: 1,
+            Rating: 1,
+            "Pure Veg": 1,
+            "Delivery Time": 1,
+            Image: 1,
+        }
+    );
+    res.status(200).json(result.slice(0, 20));
 });
+
+// app.listen(3000, () => {
+//     console.log("listing on 3k");
+// });
+// app.get("/", (req, res) => {
+//     res.status(200).json({ yougot: "rooted" });
+// });
