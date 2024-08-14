86acaab094edec98d57085fd328bdcebb58947f6
heal333 <rohanaich333@gmail.com>
2024-08-15 02:10:53 +0530

    finished pagination

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -22,109 +22,75 @@ mongoose.connect(DB).then((con) => {
 const data = mongoose.model("restaurants", new mongoose.Schema());
 
 app.get("/", (req, res) => {
-    console.log(req.query);
-
     res.status(200).json({ youGot: "rooted" });
 });
 
+const resSchema = {
+    "Restaurant Name": 1,
+    Cuisine: 1,
+    Rating: 1,
+    "Number of Ratings": 1,
+    "Pure Veg": 1,
+    "Delivery Time": 1,
+    Image: 1,
+};
+
 app.get("/restaurants", async (req, res) => {
-    // if (!req.query.highestRated && !req.query.mostRated && !req )
-    result = await data.find(
-        {},
-        {
-            "Restaurant Name": 1,
-            Cuisine: 1,
-            Rating: 1,
-            "Number of Ratings": 1,
-            "Pure Veg": 1,
-            "Delivery Time": 1,
-            Image: 1,
+    if (req.query.veg === "1") {
+        if (req.query.activeSort === "1") {
+            result = await data
+                .find({ "Pure Veg": "Yes" }, resSchema)
+                .sort({ Rating: 1 });
+            result.reverse();
+        } else if (req.query.activeSort === "2") {
+            result = await data
+                .find({ "Pure Veg": "Yes" }, resSchema)
+                .sort({ "Number of Ratings": 1 });
+            result.reverse();
+        } else if (req.query.activeSort === "3") {
+            result = await data
+                .find({ "Pure Veg": "Yes" }, resSchema)
+                .sort({ "Delivery Time": 1 });
+        } else {
+            result = await data.find({ "Pure Veg": "Yes" }, resSchema);
         }
-    );
-    res.status(200).json(result.slice(0, 20));
-});
-
-app.get("/restaurants/highestrated", async (req, res) => {
-    // console.log(req.query);
-    if (req.query.veg === "1") {
-        result = await data.find({ "Pure Veg": "Yes" }).sort({ Rating: 1 });
     } else if (req.query.veg === "2") {
-        result = await data.find({ "Pure Veg": "No" }).sort({ Rating: 1 });
+        if (req.query.activeSort === "1") {
+            result = await data
+                .find({ "Pure Veg": "No" }, resSchema)
+                .sort({ Rating: 1 });
+            result.reverse();
+        } else if (req.query.activeSort === "2") {
+            result = await data
+                .find({ "Pure Veg": "No" }, resSchema)
+                .sort({ "Number of Ratings": 1 });
+            result.reverse();
+        } else if (req.query.activeSort === "3") {
+            result = await data
+                .find({ "Pure Veg": "No" }, resSchema)
+                .sort({ "Delivery Time": 1 });
+        } else {
+            result = await data.find({ "Pure Veg": "No" }, resSchema);
+        }
     } else {
-        result = await data.find().sort({ Rating: 1 });
-    }
-    res.status(200).json(result.reverse().slice(0, 20));
-});
-
-app.get("/restaurants/mostrated", async (req, res) => {
-    if (req.query.veg === "1") {
-        result = await data
-            .find({ "Pure Veg": "Yes" })
-            .sort({ "Number of Ratings": 1 });
-    } else if (req.query.veg === "2") {
-        result = await data
-            .find({ "Pure Veg": "No" })
-            .sort({ "Number of Ratings": 1 });
-    } else {
-        result = await data.find().sort({ "Number of Ratings": 1 });
-    }
-
-    res.status(200).json(result.reverse().slice(0, 20));
-});
-
-app.get("/restaurants/deliverytime", async (req, res) => {
-    if (req.query.veg === "1") {
-        result = await data
-            .find({ "Pure Veg": "Yes" })
-            .sort({ "Delivery Time": 1 });
-    } else if (req.query.veg === "2") {
-        result = await data
-            .find({ "Pure Veg": "No" })
-            .sort({ "Delivery Time": 1 });
-    } else {
-        result = await data.find().sort({ "Delivery Time": 1 });
-    }
-
-    res.status(200).json(result.slice(0, 20));
-});
-
-app.get("/restaurants/veg", async (req, res) => {
-    if (req.query.activeSort === "1") {
-        result = await data.find({ "Pure Veg": "Yes" }).sort({ Rating: 1 });
-    } else if (req.query.activeSort === "2") {
-        result = await data
-            .find({ "Pure Veg": "Yes" })
-            .sort({ "Number of Ratings": 1 });
-    } else if (req.query.activeSort === "3") {
-        result = await data
-            .find({ "Pure Veg": "Yes" })
-            .sort({ "Delivery Time": 1 });
-    } else {
-        result = await data.find({ "Pure Veg": "Yes" });
+        if (req.query.activeSort === "1") {
+            result = await data.find({}, resSchema).sort({ Rating: 1 });
+            result.reverse();
+        } else if (req.query.activeSort === "2") {
+            result = await data
+                .find({}, resSchema)
+                .sort({ "Number of Ratings": 1 });
+            result.reverse();
+        } else if (req.query.activeSort === "3") {
+            result = await data
+                .find({}, resSchema)
+                .sort({ "Delivery Time": 1 });
+        } else {
+            result = await data.find({}, resSchema);
+        }
     }
 
-    res.status(200).json(result.reverse().slice(0, 20));
+    const pages = parseInt(req.query.page) * 20;
+    res.status(200).json(result.slice(pages - 20, pages));
+    // res.status(200).json({});
 });
-app.get("/restaurants/nonveg", async (req, res) => {
-    if (req.query.activeSort === "1") {
-        result = await data.find({ "Pure Veg": "No" }).sort({ Rating: 1 });
-    } else if (req.query.activeSort === "2") {
-        result = await data
-            .find({ "Pure Veg": "No" })
-            .sort({ "Number of Ratings": 1 });
-    } else if (req.query.activeSort === "3") {
-        result = await data
-            .find({ "Pure Veg": "No" })
-            .sort({ "Delivery Time": 1 });
-    } else {
-        result = await data.find({ "Pure Veg": "No" });
-    }
-
-    res.status(200).json(result.reverse().slice(0, 20));
-});
-// app.listen(3000, () => {
-//     console.log("listing on 3k");
-// });
-// app.get("/", (req, res) => {
-//     res.status(200).json({ yougot: "rooted" });
-// });
--- FRONTEND/src/components/body/Body.js
+++ FRONTEND/src/components/body/Body.js
@@ -1,82 +1,95 @@
 import FoodCard from "./FoodCard.js";
 import { useState, useEffect } from "react";
 
-const PageNavigation = () => {
-    return <div className="pageNavigation">1 2 3 4 5 6</div>;
-};
-
+let placeholder = [];
+for (let i = 0; i < 20; i++) {
+    placeholder.push({});
+}
 const Body = () => {
     const [activeSort, setActiveSort] = useState(0); //this can have 4 possible values, 0:no sorting, 1:sort by highest rated, 2:sort by most rated, 3:sorty by delivery time
     const [activeVeg, setActiveVeg] = useState(0); // this has 3 possible values, 0: both veg and non veg, 1:veg only, 2: nonveg only
+    const [activePage, setActivePage] = useState(1);
+    const [data, setData] = useState(placeholder);
+    const [totalPage, setTotalPage] = useState(0);
 
-    const getRes = async (api) => {
+    const getRes = async (activeSort, activeVeg, activePage) => {
         try {
-            // let response = await fetch("https://foodie-orcx.onrender.com");
-            let response = await fetch(api);
+            // let API = `http://127.0.0.1:3000/restaurants?activeSort=${activeSort}&veg=${activeVeg}&page=${activePage}`;
+            let API = `https://foodie-orcx.onrender.com/restaurants?activeSort=${activeSort}&veg=${activeVeg}&page=${activePage}`;
+
+            let response = await fetch(API);
             setData(await response.json());
         } catch (err) {
             console.log(err);
         }
     };
-    let placeholder = [];
-    for (let i = 0; i < 24; i++) {
-        placeholder.push({});
-    }
 
-    const [data, setData] = useState(placeholder);
     useEffect(() => {
-        // getRes("http://127.0.0.1:3000/restaurants");
-        getRes("https://foodie-orcx.onrender.com/restaurants");
+        getRes(activeSort, activeVeg, activePage);
+        setTotalPage(25);
     }, []);
 
     const highestRatedHandler = () => {
-        // getRes(
-        //     `http://127.0.0.1:3000/restaurants/highestrated?veg=${activeVeg}`
-        // );
-        getRes(
-            `https://foodie-orcx.onrender.com/restaurants/highestrated?veg=${activeVeg}`
-        );
         setActiveSort(1);
+        getRes(1, activeVeg, activePage);
     };
     const mostRatedHandler = () => {
-        // getRes(`http://127.0.0.1:3000/restaurants/mostrated?veg=${activeVeg}`);
-        getRes(
-            `https://foodie-orcx.onrender.com/restaurants/mostrated?veg=${activeVeg}`
-        );
         setActiveSort(2);
+        getRes(2, activeVeg, activePage);
     };
     const deliveryTimeHandler = () => {
-        // getRes(
-        //     `http://127.0.0.1:3000/restaurants/deliverytime?veg=${activeVeg}`
-        // );
-        getRes(
-            `https://foodie-orcx.onrender.com/restaurants/deliverytime?veg=${activeVeg}`
-        );
         setActiveSort(3);
+        getRes(3, activeVeg, activePage);
     };
     const vegHandler = () => {
-        // getRes(
-        //     `http://127.0.0.1:3000/restaurants/veg?activeSort=${activeSort}`
-        // );
-        getRes(
-            `https://foodie-orcx.onrender.com/restaurants/veg?aciveSort=${activeSort}`
-        );
+        if (activePage > 12) {
+            setActivePage(12);
+            getRes(activeSort, 1, 12);
+        } else {
+            getRes(activeSort, 1, activePage);
+        }
         setActiveVeg(1);
+
+        setTotalPage(12);
     };
     const nonVegHandler = () => {
-        // getRes(
-        //     `http://127.0.0.1:3000/restaurants/nonveg?activeSort=${activeSort}`
-        // );
-        getRes(
-            `https://foodie-orcx.onrender.com/restaurants/nonveg?activeSort=${activeSort}`
-        );
+        if (activePage > 14) {
+            setActivePage(14);
+            getRes(activeSort, 2, 14);
+        } else {
+            getRes(activeSort, 2, activePage);
+        }
         setActiveVeg(2);
+
+        setTotalPage(14);
     };
     const clearFilterHandler = () => {
-        getRes("https://foodie-orcx.onrender.com/restaurants");
         setActiveSort(0);
         setActiveVeg(0);
+        getRes(0, 0, activePage);
+        setTotalPage(25);
     };
+    const pageChangeHandler = (page) => {
+        setActivePage(page);
+        getRes(activeSort, activeVeg, page);
+    };
+    let pages = [];
+    let endPage = activePage + 2 > totalPage ? totalPage : activePage + 2;
+    for (
+        let i = activePage - 2 < 1 ? 1 : activePage - 2;
+        i < endPage + 1;
+        i++
+    ) {
+        pages.push(
+            <div
+                className="pageNum"
+                style={activePage === i ? { backgroundColor: "white" } : {}}
+                onClick={() => pageChangeHandler(i)}
+            >
+                {i}
+            </div>
+        );
+    }
     return (
         <div className="body">
             <div className="filterBar">
@@ -170,7 +183,39 @@ const Body = () => {
                     );
                 })}
             </div>
-            <PageNavigation />
+            <div className="pageNavigation">
+                <div className="pageNum" onClick={() => pageChangeHandler(1)}>
+                    {"<<"}
+                </div>
+                <div
+                    className="pageNum"
+                    onClick={() =>
+                        activePage > 1
+                            ? pageChangeHandler(activePage - 1)
+                            : setActivePage(1)
+                    }
+                >
+                    {"<"}
+                </div>
+                {pages}
+                <div
+                    className="pageNum"
+                    onClick={() =>
+                        activePage < totalPage
+                            ? pageChangeHandler(activePage + 1)
+                            : setActivePage(activePage)
+                    }
+                >
+                    {">"}
+                </div>
+                <div
+                    className="pageNum"
+                    onClick={() => pageChangeHandler(totalPage)}
+                >
+                    {">>"}
+                </div>
+            </div>
+            ;
         </div>
     );
 };
