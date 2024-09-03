de75b33f453857d4ffa8cb54d0f249ec0006da4f
heal333 <rohanaich333@gmail.com>
2024-09-03 08:23:57 +0530

    Updates

--- index.7271efb6.js
+++ index.7271efb6.js
@@ -34893,8 +34893,8 @@ const ThemeButton = ()=>{
     _s();
     const [darkTheme, setDarkTheme] = (0, _react.useState)(false);
     const switchThemeHandler = ()=>{
-        if (darkTheme) root.style.cssText = "--headerBackground: white; --bodyBackground: rgb(230, 230, 230); --cardBackground: white; --textColor: black; --softTextColor: rgb(80,80,80); --theme: rgb(0, 212, 0); background-color: var(--bodyBackground);";
-        else root.style.cssText = "--headerBackground: rgb(50,50,50); --bodyBackground: black; --cardBackground: rgb(50,50,50); --textColor: white; --softTextColor: rgb(200,200,200); --theme: rgb(0, 212, 0); background-color: var(--bodyBackground);";
+        if (darkTheme) root.style.cssText = "--headerBackground: white; --bodyBackground: rgb(230, 230, 230); --cardBackground: white; --textColor: black; --softTextColor: rgb(80,80,80); --theme: rgb(0, 212, 0); background-color: var(--bodyBackground); --shadowColor: gray;";
+        else root.style.cssText = "--headerBackground: rgb(50,50,50); --bodyBackground: black; --cardBackground: rgb(50,50,50); --textColor: white; --softTextColor: rgb(200,200,200); --theme: rgb(0, 212, 0); background-color: var(--bodyBackground); --shadowColor: rgba(0,212,0,.5);";
         setDarkTheme(!darkTheme);
     };
     let root = document.getElementById("root");
